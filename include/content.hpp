551a1b7d9a5b8370580d2d05c8c7b19e4ba63b02
Ruslan Yevdokymov <38809160+ruslanye@users.noreply.github.com>
2021-01-11 12:30:49 +0100

    Implement content (#8)
    * Implement content
    
    * Nitpicking prevention
    
    * Move MessageType to separate file
    
    * Add missing newline
    
    * Rename MessageType file

--- include/content.hpp
+++ include/content.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
-class Content {
+#include "message_type.hpp"
+#include "node/block.hpp"
 
+struct Content {
+    const MessageType messageType;
+    const Block block;
+    Content(MessageType messageType, Block block);
 };
