f081d74e106f26efd2183cf54d7bf063d75d6dd6
kostekIV <27210860+kostekIV@users.noreply.github.com>
2021-01-12 12:39:25 +0100

    Add logs (#13)

--- src/node/dummy_node.cpp
+++ src/node/dummy_node.cpp
@@ -1,12 +1,16 @@
+
+#include "logging/easylogging++.h"
 #include "node/dummy_node.hpp"
 
 
 std::vector<Message> DummyNode::onMessageReceive(const Message& message) {
     // do nothing
+    LOG(DEBUG) << "Dummy Node: received message from " << message.from();
     return std::vector<Message>{};
 }
 
 std::vector<Message> DummyNode::atTime(unsigned t) {
     // do nothing
+    LOG(DEBUG) << "Dummy Node: received atTime: " << t;
     return std::vector<Message>{};
 }
--- src/state/tree/tree.cpp
+++ src/state/tree/tree.cpp
@@ -1,8 +1,19 @@
 #include <algorithm>
 #include <utility>
+
+#include "logging/easylogging++.h"
+
 #include "state/tree/tree.hpp"
 
 #define range(V) (V).begin(), (V).end()
+
+namespace {
+    void checkAndLogError(std::unordered_map<hash_t, const Vertex> &hvMapping, hash_t key) {
+        if (hvMapping.find(key) == hvMapping.end()) {
+            LOG(ERROR) << "No key " << key << "in tree map";
+        }
+    }
+}
 
 Tree::Tree(const Hashable& rootContent, pred_t finalizationPredicate) :
         finalizationPredicate(std::move(finalizationPredicate)),
@@ -13,21 +24,27 @@ Tree::Tree(const Hashable& rootContent, pred_t finalizationPredicate) :
     auto& root = hvMapping.at(rootHash);
     root.notarize();
     root.finalize();
+
+    LOG(DEBUG) << "[TREE]: " << "Initialised with \"" << root.getContent().hash() << "\" as root hash";
 }
 
 bool Tree::isRoot(const Vertex& v) { return v.getDepth() == 0; }
 
 const Hashable& Tree::getDeepestNotarized() const {
+    LOG(DEBUG) << "[TREE]: " << "getDeepestNotirized";
     return deepestNotarized.get().getContent();
 }
 
 void Tree::addBelow(const Hashable& parent, const Hashable& child) {
+    LOG(DEBUG) << "[TREE]: " << "addBelow parent with hash " << parent.hash() << " child with hash " << child.hash();
     const Vertex& parentVertex = hvMapping.at(parent.hash());
     hash_t childHash = child.hash();
     hvMapping.try_emplace(childHash, child, parentVertex);
 }
 
 void Tree::notarize(const Hashable& hashable) {
+    LOG(DEBUG) << "[TREE]: " << "notarize block with hash " << hashable.hash();
+    checkAndLogError(hvMapping, hashable.hash());
     const Vertex& v = hvMapping.at(hashable.hash());
     v.notarize();
     if (v.getDepth() > deepestNotarized.get().getDepth())
@@ -36,6 +53,7 @@ void Tree::notarize(const Hashable& hashable) {
 }
 
 void Tree::tryFinalizeUntil(const Vertex& v) {
+    LOG(DEBUG) << "[TREE]: " << "tryFinalizeUntil vertex with hash " << v.getContent().hash();
     auto path = getPathFromRootTo(v);
     hashables_t hashables;
     std::for_each(range(path), [&hashables](auto v) { hashables.push_back(std::cref(v.get().getContent())); });
