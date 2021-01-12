583d4e64f23d8975126d2af1e4e767ce43d289a4
Ruslan Yevdokymov <evruslan17@gmail.com>
2021-01-12 04:07:30 +0100

    Implement honest node

---
+++ include/node/honest_node.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+#include <unordered_map>
+
+#include "state/tree/tree.hpp"
+#include "hashservice.hpp"
+#include "node/node.hpp"
+
+class HonestNode : public INode{
+public:
+    HonestNode(unsigned id, unsigned numOfNodes, HashService service, Block genesisBlock);
+    std::vector<Message> onMessageReceive(const Message& message) override;
+    std::vector<Message> atTime(unsigned t) override;
+private:
+    static const Block& castFromHashable(const std::reference_wrapper<const Hashable>& ref);
+    static bool finalizationPredicate(const std::vector<std::reference_wrapper<const Hashable>>& blocks);
+    std::vector<Message> broadcast(Content content);
+
+    unsigned id;
+    unsigned numOfNodes;
+    HashService service;
+    Tree tree;
+    std::unordered_map<hash_t, int> votes;
+
+    static unsigned EPOCH_LENGTH;
+};
