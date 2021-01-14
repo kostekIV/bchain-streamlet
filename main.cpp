231ba5ce226969a4b341d96a3b1db1e6b58fbdf7
KostekIV <koscis.j@gmail.com>
2021-01-14 22:27:30 +0100

    Add possibility to disable logs.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,18 @@ INITIALIZE_EASYLOGGINGPP
 
 int main(int argc, char **argv) {
     START_EASYLOGGINGPP(argc, argv);
-    LOG(INFO) << "My first info log using default logger";
+
+    el::Configurations defaultConf;
+    std::string enabled = "true";
+
+    // to disable logging pass "false" as first argument
+    if (argc > 1) {
+        enabled = argv[1];
+    }
+
+    defaultConf.setGlobally(
+                el::ConfigurationType::Enabled, enabled);
+    el::Loggers::reconfigureLogger("default", defaultConf);
 
     DummyNode node;
     Message Message{0, 0, {MessageType::PROPOSAL, {"", 0, ""}}};
